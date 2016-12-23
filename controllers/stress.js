1f893557761b7b15a4323a8e2456d2dd6ecb0713
Angelos Alexopoulos <alexopoulos7@gmail.com>
2016-12-23 16:50:30 +0200

    Break requests to chunks of 1000 for memory issues

--- controllers/stress.js
+++ controllers/stress.js
@@ -1,6 +1,7 @@
 'use strict';
 const async = require('async');
 const request_retry = require('requestretry')
+const debug = require('debug')('stress')
 
 module.exports = (req) => {
 	//console.log(`Parameters are ${JSON.stringify(req.body)}`);
@@ -15,34 +16,54 @@ module.exports = (req) => {
 		let matchWord = req.body.matchWord;
 		let matchWordReplace = (req.body.matchWordReplace || "false" ) === "true";
 
-		let toSendRequests = [];
+		
 		let re = new RegExp(matchWord, 'g');
-		for (var i = 0; i < reqs; i++) {
-			let bodyText = "";
-			let rand = Math.floor(Math.random() * 90000) + 10000;
-			if (method !== 'GET' && requestBody && requestBody.length && matchWordReplace){
-				rand = rand + i 
-				bodyText = requestBody.replace(re, rand.toString());
+		// lets break requests to 1000 chunks
+		const chunkSize = reqs>1000?1000:reqs;
+		let chunks = Math.floor(reqs / chunkSize);
+		debug('total number of chunks '+chunks);
+		for (let c =0; c<=chunks; c++ ){
+
+			let chunkStart = c*chunkSize;
+			let chunkEnd = (c+1)*chunkSize;
+			if (c===chunks){
+				chunkEnd = reqs;
 			}
-			let headers = {}
-			if (header && headerValue){
-				headers = [{name:header, value:headerValue}]
+			debug('Chunk Start '+chunkStart);
+			debug('Chunk End '+chunkEnd);
+			let toSendRequests = [];
+			for (let i = chunkStart; i < chunkEnd; i++) {
+				debug(i);
+				let bodyText = "";
+				let rand = Math.floor(Math.random() * 90000) + 10000;
+				if (method !== 'GET' && requestBody && requestBody.length && matchWordReplace){
+					rand = rand + i 
+					bodyText = requestBody.replace(re, rand.toString());
+				}
+				let headers = {}
+				if (header && headerValue){
+					headers = [{name:header, value:headerValue}]
+				}
+				let options = {
+					url :url,
+					method:method,
+					headers:headers,
+					body: bodyText
+					
+				}
+			    toSendRequests.push(options);
 			}
-			let options = {
-				url :url,
-				method:method,
-				headers:headers,
-				body: bodyText
-				
-			}
-		    toSendRequests.push(options);
+			async.map(toSendRequests, request_retry, (err,results)=>{
+				if (err){
+					debug('Error! '+ err);
+				}
+				// console.log(results);
+					debug('done chunk '+c);
+			});
 		}
-		async.map(toSendRequests, request_retry, (err,results)=>{
-			if (err){
-				console.log('Error! '+ err);
-			}
-			console.log(results);
-		});
+			
+
+		
 
 		return `${reqs} Requests started to flow`
 
